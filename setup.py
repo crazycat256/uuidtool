8906b4ee5bf58e7d51cc51d0a26afab1d512f6e1
crazycat256 <63475640+crazycat256@users.noreply.github.com>
2025-02-16 16:03:33 +0100

    Add project details

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="1.0",
     description="CLI tool to manipulate UUIDs",
     author="crazycat256",
+    url="https://github.com/crazycat256/uuidtool",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     entry_points={
@@ -13,4 +14,12 @@ setup(
         ],
     },
     python_requires=">=3.10",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
